dd34ca76ca9c8d18a80af992609b6115799fe335
zeesairafi <alsairafizainab19@gmail.com>
2022-02-07 14:13:34 +0300

    details

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Detail = () => {
+const Detail = (props) => {
   return (
     <div>
       <div className="header-dark">
@@ -9,8 +9,34 @@ const Detail = () => {
         </div>
       </div>
       <div className="in-body-detail">
-        <div></div>
+        <div>
+        <img  src="https://hips.hearstapps.com/hmg-prod/images/delish-bucatinipasta-028-ls-1607552701.jpg"/>
+</div>
+        <div><h2>Need More Everyday Cooking Tips? Here Are 23 Of Our Favorites</h2></div>
       </div>
+      <div>
+      <img className="img" alt="avatar" src="https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png" /> <p>User Name</p></div>
+    <div>
+      <h2>Ingredients</h2>
+     
+      <p>2 cloves garlic
+</p> 
+<p>10 g parsley
+</p>
+ <p>100 g orzo</p>
+ <p>250 ml vegetable stock
+</p> 
+<p>2 cloves 	garlic
+</p>
+      </div>
+      <div>
+        <h2>Discretions</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+      </div>
+<div>
+      <button>Back</button>
+      </div>
+
     </div>
   );
 };
