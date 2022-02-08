8f2d05156828fded8a815b6b0b2367594cf4ab84
a1000rhan <1000rhan@gmail.com>
2022-02-08 08:44:18 +0300

    userprofile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "./recipe.css";
 import "./detail.css";
+import "./userprofile.css";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Detail from "./components/Detail";
 import Recipes from "./components/Recipes";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/detail" element={<Detail />} />
+        <Route path="/userprofile" element={<UserProfile />} />
       </Routes>
     </dic>
   );
---
+++ src/components/UserProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserProfile = () => {
+  return (
+    <div>
+      <div className="header-nav-profi"></div>
+      <h1 className="title-user">User Profile</h1>
+      <div className="in-body-detail"></div>
+    </div>
+  );
+};
+
+export default UserProfile;
