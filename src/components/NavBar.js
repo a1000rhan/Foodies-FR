90354a62f64c970b19a9c56dd8f8cedc76fc72e1
a1000rhan <1000rhan@gmail.com>
2022-02-08 09:33:05 +0300

    profile page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import UserModal from "./UserModal";
+import { observer } from "mobx-react";
 
 const NavBar = () => {
   const [query, setQuery] = useState("");
@@ -54,4 +55,4 @@ const NavBar = () => {
   );
 };
 
-export default NavBar;
+export default observer(NavBar);
