9388431317200d488186090aaef60aa52ae75c23
a1000rhan <1000rhan@gmail.com>
2022-02-10 10:32:44 +0300

    update

--- src/components/RecipeModal.js
+++ src/components/RecipeModal.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { Modal, Form, FormControl } from "react-bootstrap";
 import { MdFoodBank } from "react-icons/md";
-import Categorie from "../Categories";
-import Ingredients from "../Ingredients";
 import authstore from "../store/authStore";
 import recipeStore from "../store/recipeStore";
 import { observer } from "mobx-react";
@@ -11,9 +9,12 @@ import ingredientStore from "../store/ingredientStore";
 
 const RecipeModal = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const [categoryValue, setCategoryValue] = useState("Italian");
   const [chooseCato, setChooseCato] = useState([]);
+  console.log(
+    "🚀 ~ file: RecipeModal.js ~ line 17 ~ RecipeModal ~ chooseCato",
+    chooseCato
+  );
   const [newCat, setNewCat] = useState("");
 
   const [ingValue, setIngValue] = useState("lemon");
@@ -25,19 +26,17 @@ const RecipeModal = () => {
     image: "",
     discerption: "",
     calories: "",
-    category: "",
-    ingredients: "",
+    category: [],
+    ingredients: ["620381371e52e4e8912555e6", "6203813e1e52e4e8912555ed"],
   });
+  console.log(
+    "🚀 ~ file: RecipeModal.js ~ line 24 ~ RecipeModal ~ recipe",
+    recipe
+  );
 
+  if (categoryStore.loading) return <h1>loading</h1>;
   const handleChange = (event) => {
     setRecipe({ ...recipe, [event.target.name]: event.target.value });
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    recipeStore.createRecipe(recipe);
-
-    setIsOpen(false);
   };
 
   //Add to Category Array
@@ -49,9 +48,9 @@ const RecipeModal = () => {
     const myCato = categoryStore.category.find(
       (ca) => categoryValue === ca.title
     );
-    setChooseCato([...chooseCato, myCato.title]);
+    setChooseCato([...chooseCato, myCato]);
 
-    setRecipe({ ...recipe, category: myCato._id });
+    setRecipe({ ...recipe, category: chooseCato.map((cat) => cat._id) });
     //add
   };
 
@@ -96,16 +95,33 @@ const RecipeModal = () => {
   const ing = ingredientStore.ingredient.map((ca) => (
     <option value={ca.name}>{ca.name}</option>
   ));
+
   const categoryList = chooseCato.map((cato) => (
     <div>
-      <i>{cato}</i>
+      <i>{cato.title}</i>
     </div>
   ));
+
   const ingredientsList = chooseIng.map((ingr) => (
     <div>
       <i>{ingr}</i>
     </div>
   ));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    recipeStore.createRecipe(recipe);
+    setRecipe({
+      title: "",
+      image: "",
+      discerption: "",
+      calories: "",
+      category: [],
+      ingredients: [],
+    });
+
+    setIsOpen(false);
+  };
   return (
     <div>
       {authstore.user ? (
